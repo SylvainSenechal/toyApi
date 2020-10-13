c2860c7ebdb75b04973fd834d8da1852155d143c
SylvainSenechal <senechalsylvain@hotmail.com>
2020-10-14 01:42:13 +0200

    refactoring + edge cases

--- tools.js
+++ tools.js
@@ -1,84 +1,91 @@
 // TODO: verifier double \n et les curl vs text
-// edge case : empty line, word > 80, weird paragragp structure
+// edge case : empty line, word > 80, weird paragragh structure
+// word > 2*limit
 
 const justifyText = (text, neededLength = 80) => {
-  // First we assign each word to a line
-  // Then we pad each line (except the last one) with an extra space until we get 80 characters lines
+  // 2 Steps :
+  // - First We assign each word to it's line
+  // - Second We pad each line (except the last one) with an extra space until
+  // we get 80 characters lines
 
-  // let text = `bonjour comment puis je t aider
-  // ça va bien ce soir ?`
+  let paragraphs = text.split('\n') // We will treat each paragraph independently
+  let processedParagraphs = []
 
-
-  let paragraphs = text.split('\n')
-  // console.log(paragraphs)
-  console.log('START')
-  let result = []
   for (let paragraph of paragraphs) {
-    console.log('new paragraph')
-    let words = paragraph.trim().split(' ') // We get each words of the paragraph and we greedily add them to our lines
-    // console.log(words)
-    let lines = []
-    let currentLine = []
-    let currentLineLength = 0
-    for (let word of words) {
-      if (currentLineLength + word.length <= neededLength) {
-        currentLine.push(word)
-        currentLineLength += word.length + 1
+    // We get each words of the paragraph and we greedily add them to our lines
+    let words = paragraph.trim().split(' ')
+    let lines = buildLinesFromWords(words, neededLength)
+    processedParagraphs.push(lines)
+  }
+
+
+  let justifiedText = ''
+  processedParagraphs.forEach((paragraph, idParagraph) => {
+    paragraph.forEach((line, id) => {
+      if (id === paragraph.length - 1) { // We don't pad the last line of a paragraph
+        justifiedText += line.join(' ')
       } else {
+        justifiedText += padLine(line, neededLength)
+      }
+      // New paragraph except on the last one
+      if (idParagraph !== processedParagraphs.length - 1) justifiedText += '\n'
+    })
+  })
+  return justifiedText
+}
+
+const buildLinesFromWords = (words, neededLength) => {
+  let lines = []
+  let currentLine = []
+  let currentLineLength = 0
+  for (let word of words) {
+    if (currentLineLength + word.length <= neededLength) { // Adding words greedily
+      currentLine.push(word)
+      currentLineLength += word.length + 1
+    } else {
+      // Dealing with words longer than max line width
+      if (word.length > neededLength) {
+        let lengthTooLongWord = word.length
+        lines.push(currentLine)
+        for (let i = 0; i < Math.floor(lengthTooLongWord / neededLength); i++) {
+          lines.push([word.slice(i * neededLength, (i + 1) * neededLength)])
+        }
+        let endOfWord = word.slice(Math.floor(lengthTooLongWord / neededLength) * neededLength, lengthTooLongWord)
+        if (endOfWord.length !== 0) { // We don't want to add the empty word : ''
+          currentLine = [endOfWord]
+          currentLineLength = endOfWord.length + 1
+        } else {
+          currentLine = []
+          currentLineLength = 0
+        }
+      } else { // When the current line is filled, we create the next one
         lines.push(currentLine)
         currentLine = [word]
         currentLineLength = word.length + 1
       }
-      // console.log('new iter')
-      // console.log(lines)
-      // console.log(currentLine)
-      // console.log(currentLineLength)
     }
-    lines.push(currentLine)
-    console.log('End paragraph')
-    // console.log(lines)
-    // console.log(currentLine)
-    // console.log(currentLineLength)
-    result.push(lines)
-
-    // console.log(result)
   }
-  // console.log(result)
+  if (currentLine.length > 0) lines.push(currentLine)
 
-  let justifiedText = ''
-
-  result.forEach(paragraph => {
-    paragraph.forEach((line, id) => {
-      if (id === paragraph.length - 1) { // We don't pad the last line of a paragraph
-        justifiedText += line.join(' ') + '\n'
-      } else {
-        justifiedText += padLine(line, neededLength) + '\n'
-      }
-    })
-  })
-
-  // console.log(justifiedText)
-  return justifiedText
-// if (character.charCodeAt() === 10)
+  return lines
 }
 
 const padLine = (line, neededLength) => {
-  // console.log('line : ', line)
-  if (line.length === 1) { // If we only have one word to pad, we just add the missing whitespace at the end
-    line[0] = line[0].padEnd(15, ' ')
+  if (line.length === 1) { // If we only have one word to pad, we just add the missing whitespaces at the end
+    line[0] = line[0].padEnd(neededLength, ' ')
   } else {
     let lineLenght = line.reduce((acc, word) => word.length + acc + 1, 0) - 1 // -1 there is no whitespace for the last word
     let missingWhitespaces = neededLength - lineLenght
     indexNextNewWhitespace = 0
+
+    // We are adding a whitespace after each word of the line from left to right
+    // We use : % (line.length - 1) because we might have to add to
+    // add several whitespace, and (line.length - 1) because we don't want to add a whitespace
+    // to the last word
     while (missingWhitespaces !== 0) {
       line[indexNextNewWhitespace] += ' '
-      missingWhitespaces--
-
-      // We are adding a whitespace after each word of the line from left to right
-      // We use : % (line.length - 1) because we might have to add to
-      // add several whitespace, line.length - 1 because we don't want to add a whitespace
-      // to the last word
       indexNextNewWhitespace = (indexNextNewWhitespace + 1) % (line.length - 1)
+      missingWhitespaces--
     }
   }
 
